5cf9706503f31111d42c10f6b5ba2304540f3f96
Emir Risyad <emirrsyd@gmail.com>
2026-05-25 21:01:30 +0200

    fix: adjust items position on inventory

--- components/inventory_ui.gd
+++ components/inventory_ui.gd
@@ -1,10 +1,19 @@
 extends Control
 
+@onready var inventory: Inventory = preload("res://data/inventory/player_inventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open: bool = false
 
 
 func _ready() -> void:
+	update_slots()
 	close()
+
+
+func update_slots() -> void:
+	for i in range(min(inventory.items.size(), slots.size())):
+		slots[i].update(inventory.items[i])
 
 
 func _process(_delta) -> void:
