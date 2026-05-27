4bd126424c7daadb3cd4480315380bac9a92ba26
starry-nites <jacjack09@outlook.com>
2026-05-27 12:34:04 -0500

    No more Spaghetti code. That shit was bad. It worked, but not very well.

--- scripts/main-menu/main_menu_buttons.gd
+++ scripts/main-menu/main_menu_buttons.gd
@@ -3,6 +3,7 @@ extends Node
 const QuickTimeEvent = preload("res://scripts/quick-time-events/quick_time_event.gd")
 var QTE: quick_time_event
 var x: ColorRect
+var node: Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var button = Button.new()
@@ -10,30 +11,16 @@ func _ready() -> void:
 	button.pressed.connect(_button_pressed)
 	add_child(button)
 	
-	var spwnpb = Button.new()
-	spwnpb.position = Vector2(20,40)
-	spwnpb.text = "spawn pebble"
-	add_child(spwnpb)
-	
-	QTE = quick_time_event.new()
-	
-	spwnpb.pressed.connect(_spawn_pebble)
-	
-func _spawn_pebble():
-	print("should spawn pebble")
-	var node = get_node("Control")
-	
-	x = QTE.create(100, 10, 10, 10, 200, node)
-	#QTE.set_pebble(x)
+	node = get_node("Control")
+	QTE = quick_time_event.new(100, 10, 10, 10, 200, node)
 	
 	
-	#add_child(x)
-	
+
 func _button_pressed():
 	get_tree().change_scene_to_file("res://scenes/shop_scenes/shop.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (x != null):
-		QTE.move_pebble(x, delta)
+	if (QTE.pebble != null):
+		QTE.move_pebble(QTE.pebble, delta)
--- scripts/quick-time-events/quick_time_event.gd
+++ scripts/quick-time-events/quick_time_event.gd
@@ -12,37 +12,43 @@ var pebble_bound_right: float
 var dir = "right" # Dictates which direction the pebble is moving. The pebble will start moving right by default
 
 # Called when the node enters the scene tree for the first time.
-func create(s: float, ps: float, gs: float, pbl: float, pbr: float, node: Node) -> ColorRect:
-	print("Is it spawned yet?")
+
+# INFO 
+ ## TO CREATE A QUICK TIME EVENT:
+ ## Float: SPEED; Used to determine how quickly the pebble moves across the screen, in pixels (per frame).
+ ## Float: SIZE; Used to create the pebble's horizontal size, in pixels.
+ ## Float: GREEN_SIZE; Determines how large the "green area" is inside the QTE, in pixels.
+ ## Float: PEBBLE_BOUND_LEFT; Determines where on the left-hand side of the screen the pebble will "bounce" 
+ 	## back on. Creates an invisible wall at the specified x-value that sends the pebble the other direction, 
+ 	## when the pebble strikes it.
+ ## Float: PEBBLE_BOUND_RIGHT; Same as Pebble_bound_left, but instead determines the right-hand side bound.
+ ## Node: NODE; Determines which node the QTE and pebble should be children of.
+func _init(s: float, ps: float, gs: float, pbl: float, pbr: float, node: Node) -> void:
 	
-	var QTE = quick_time_event.new()
+	# Just creating a new rectangle and instantiating all of my variables
 	pebble = ColorRect.new()
-	
-	QTE.speed = s
 	speed = s
-	QTE.pebble_size = ps
-	QTE.green_size = gs
+	pebble_size = ps
+	green_size = gs
 	
-	QTE.pebble_bound_left = pbl
 	pebble_bound_left = pbl
-	QTE.pebble_bound_right = pbr
 	pebble_bound_right = pbr
 	
-	pebble.color = Color(1, 0, 0) # Red color
-	pebble.size = Vector2(ps, ps) # Size in pixels
-	pebble.position = Vector2(pbl, 100) # Position on screen
+	pebble.color = Color(1, 0, 0)
+	pebble.size = Vector2(40, ps)
+	pebble.position = Vector2(pbl, 100)
 	
-	node.add_child(pebble) # Makes the pebble visible
-	node.add_child(QTE) # Makes the QTE accessible from the scene and runs _ready() and technically _process()
+	node.add_child(pebble)
+	node.add_child(self)
 	
-	return pebble
+	# Moves the pebble. When it hits the X-Value from PBL, it will turn around and begin to move right, 
+	#	 and vice versa.
 	
-func set_pebble(rect: ColorRect):
-	print("set the pebble")
-	pebble = rect
-	
+	# Delta is sent from the _process() function from which this move_pebble() is called.
+	# DEPRECATED ColorRect is the specific rectangle to move
 func move_pebble(x: ColorRect, delta: float) -> void:
 	
+	# Checks if we have hit either bound. If so, switch to the other direction.
 #region New Code Region
 	if (x.position.x >= pebble_bound_right):
 		dir = "left"
@@ -53,16 +59,14 @@ func move_pebble(x: ColorRect, delta: float) -> void:
 		print("I Changed direction to right")
 #endregion
 	
+	# Depending on whatever direction we are supposed to be going, we either add or subtract to or from 
+	# 	the pebble's X-Coordinate. 
 #region New Code Region
 	if dir == "right":
-		x.position.x += speed * delta
+		x.position.x += speed * delta # Delta here used in order to scale movement to the frame rate.
 		print(speed)
 	
 	elif dir == "left":
 		x.position.x -= speed * delta
 		print("I moved left")
 #endregion
-
-func _process(delta: float) -> void:
-	#move_pebble()
-	pass
