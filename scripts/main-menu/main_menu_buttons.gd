b053558b5e6f49602c4b10ba7a33f64e478f3137
starry-nites <jacjack09@outlook.com>
2026-05-27 21:14:46 -0500

    Added randomized motion. Can now use either random or standard motion for pebble

--- scripts/main-menu/main_menu_buttons.gd
+++ scripts/main-menu/main_menu_buttons.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	add_child(button)
 	
 	node = get_node("Control")
-	QTE = quick_time_event.new(100, 10, 10, 10, 200, node)
+	QTE = quick_time_event.new(200, 10, 10, -20, 400, 5, node)
 	
 	
 
@@ -23,4 +23,4 @@ func _button_pressed():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if (QTE.pebble != null):
-		QTE.move_pebble(QTE.pebble, delta)
+		QTE.move_pebble_random(delta)
--- scripts/quick-time-events/quick_time_event.gd
+++ scripts/quick-time-events/quick_time_event.gd
@@ -9,7 +9,12 @@ var pebble: ColorRect
 var pebble_bound_left: float
 var pebble_bound_right: float
 
+var interval: float
+
 var dir = "right" # Dictates which direction the pebble is moving. The pebble will start moving right by default
+
+var timer_num: float # Determines how long we will run the program before switching directions assuming base case.
+var direction_num: int # Acts as a coinflip to decide if we are moving left or right.
 
 # Called when the node enters the scene tree for the first time.
 
@@ -23,7 +28,7 @@ var dir = "right" # Dictates which direction the pebble is moving. The pebble wi
  	## when the pebble strikes it.
  ## Float: PEBBLE_BOUND_RIGHT; Same as Pebble_bound_left, but instead determines the right-hand side bound.
  ## Node: NODE; Determines which node the QTE and pebble should be children of.
-func _init(s: float, ps: float, gs: float, pbl: float, pbr: float, node: Node) -> void:
+func _init(s: float, ps: float, gs: float, pbl: float, pbr: float, i: float, node: Node) -> void:
 	
 	# Just creating a new rectangle and instantiating all of my variables
 	pebble = ColorRect.new()
@@ -34,27 +39,32 @@ func _init(s: float, ps: float, gs: float, pbl: float, pbr: float, node: Node) -
 	pebble_bound_left = pbl
 	pebble_bound_right = pbr
 	
+	interval = i
+	
 	pebble.color = Color(1, 0, 0)
 	pebble.size = Vector2(40, ps)
-	pebble.position = Vector2(pbl, 100)
+	pebble.position = Vector2((pbl+pbr)/2, 100)
 	
 	node.add_child(pebble)
 	node.add_child(self)
+	
+	timer_num = randf_range(1, interval) # Used later for pebble movement. See pebble_move_random.
+	direction_num = 1
+	
 	
 	# Moves the pebble. When it hits the X-Value from PBL, it will turn around and begin to move right, 
 	#	 and vice versa.
 	
 	# Delta is sent from the _process() function from which this move_pebble() is called.
-	# DEPRECATED ColorRect is the specific rectangle to move
-func move_pebble(x: ColorRect, delta: float) -> void:
+func move_pebble_linear(delta: float) -> void:
 	
 	# Checks if we have hit either bound. If so, switch to the other direction.
 #region New Code Region
-	if (x.position.x >= pebble_bound_right):
+	if (pebble.position.x >= pebble_bound_right):
 		dir = "left"
 		print("I Changed direction to left")
 		
-	if (x.position.x <= pebble_bound_left):
+	if (pebble.position.x <= pebble_bound_left):
 		dir = "right"
 		print("I Changed direction to right")
 #endregion
@@ -63,10 +73,51 @@ func move_pebble(x: ColorRect, delta: float) -> void:
 	# 	the pebble's X-Coordinate. 
 #region New Code Region
 	if dir == "right":
-		x.position.x += speed * delta # Delta here used in order to scale movement to the frame rate.
+		pebble.position.x += speed * delta # Delta here used in order to scale movement to the frame rate.
 		print(speed)
 	
 	elif dir == "left":
-		x.position.x -= speed * delta
+		pebble.position.x -= speed * delta
 		print("I moved left")
 #endregion
+
+# Should be called once every frame by a _process() function. Slides the pebble around in 
+# 	alternating directions for random periods of time.
+func move_pebble_random(delta: float) -> void:
+	
+	# Velocity includes the speed at which the pebble travels, direction it is traveling, and 
+	# 	the delta to scale for the framerate. Instead of applying these values manually, we add this vector instead
+	var velocity = (speed * direction_num * delta)
+	
+	# If the anticipated movement would put us at the bounds, turn the other direction instead.
+	if (pebble.position.x + velocity >= pebble_bound_right || pebble.position.x + velocity <= pebble_bound_left):
+		direction_num *= -1
+		pebble.position.x += velocity
+		return
+	
+	# Move the pebble. 
+	pebble.position.x += velocity
+	
+	# If we have not yet run out of time, continue to keep the timer, and escape the function for now
+	if (timer_num > 0):
+		timer_num -= delta # Keeps track of how much real-world time has passed.
+		return
+	
+	# Since we have obviously now run out of time, replenish the time to some random number.
+	timer_num = randf_range(1, interval)
+	
+	# Once we have run out of time, change the pebble's direction as well.
+	direction_num *= -1
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
